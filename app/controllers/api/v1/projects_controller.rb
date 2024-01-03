d8672a54cd985ec5479b7d65d960be6be58462ce
samojeyinka <samuelojeyinka@gmail.com>
2024-01-03 00:34:17 -1200

    started the project ft,created the model,routes and controller

---
+++ app/controllers/api/v1/projects_controller.rb
@@ -0,0 +1,49 @@
+class Api::V1::ProjectsController < ApplicationController
+    before_action :find_project, only: [:show, :update, :destroy]
+def index
+    @projects = Project.all
+    render json: @projects
+end
+
+
+def show
+    render json: @project
+end
+
+def create
+    @project = Project.new(project_params)
+    if @project.save
+        render json: @project
+    else
+        render json: @project.errors.full_messages, status: :unprocessable_entity
+    end
+end
+
+def update
+    if @project.update(project_params)
+        render json: @project
+    else
+        render json: @project.errors.full_messages, status: :unprocessable_entity
+    end
+
+end
+
+def destroy
+    if @project.destroy
+        render json: { message: "Deleted successfully" }, status: :ok
+    else
+        render json: { error: "Unable to delete project" }, status: :unprocessable_entity
+    end
+end
+
+private
+
+def find_project 
+    @project = Project.find(params[:id])
+end   
+
+def project_params
+    params.require(:project).permit(:name, :description, :details)
+end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_03_042548) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_03_121321) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
