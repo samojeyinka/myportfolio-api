da7fdcb0783f078d17c2d13f07e6e304c0572d89
samojeyinka <samuelojeyinka@gmail.com>
2024-01-02 20:26:01 -1200

    Created the article controller

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,54 @@
+module Api
+    module V1
+      class ArticlesController < ApplicationController
+        before_action :find_article, only: [:show, :update, :destroy]
+
+        def index
+         @articles = Article.all
+         render json: @articles
+        end
+
+        def show
+          render json: @article
+        end
+
+        def create
+          @article = Article.new(article_params)
+          if @article.save
+            render json: @article
+          else
+              render json: @article.errors.full_messages, status: :unprocessable_entity
+          end
+        end
+
+        def update
+          if @article.update(article_params)
+            render json: @article
+          else
+              render json: @article.errors.full_messages, status: :unprocessable_entity
+          end
+        end
+
+        def destroy
+          if @article.destroy
+            render json: { message: "Deleted successfully" }, status: :ok
+          else
+            render json: { error: "Unable to delete article" }, status: :unprocessable_entity
+          end
+        end
+        
+        private
+
+        def find_article
+          @article = Article.find(params[:id])
+        end
+
+
+        def article_params
+          params.require(:article).permit(:title, :content)
+        end
+        
+      end
+    end
+  end
+  
