dd5f02b64ecff842dcbb5a3e51210aa67e94508e
samojeyinka <samuelojeyinka@gmail.com>
2024-01-02 23:20:44 -1200

    Added validtions to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,12 @@
 class Article < ApplicationRecord
+    before_validation :strip_whitespace
+
+    validates :title, presence: true
+    validates :content, presence:true
+
+    def strip_whitespace
+        self.title = title.strip
+        self.content = content.strip
+    end
+
 end
