2895ad2fa17532a3e51b28b030e8df961ca3842e
samojeyinka <samuelojeyinka@gmail.com>
2024-01-03 00:45:33 -1200

    added validations to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,14 @@
 class Project < ApplicationRecord
+
+    before_validation :strip_whitespace
+
+    validates :name, presence: true, length:{maximum: 100}
+    validates :description, presence: true, length:{maximum: 140}
+    validates :details, presence: true
+
+    def strip_whitespace
+        self.name = name.strip if name.present?
+        self.description = description.strip if description.present?
+        self.details = details.strip if details.present?
+    end
 end
